7a4385c5833a545a6752afaee207db192a1846ee
Kelly Nelson <kelnel86@gmail.com>
2022-09-22 17:57:59 -0500

    makes sure all components are working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {browserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Stocks from './pages/Stocks';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <About />
+      <Dashboard />
+      <Home />
+      <Stocks />
       
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <h1>This is the Nav Component</h1>
+    )
+}
+
+export default Nav;
