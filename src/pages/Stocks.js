3cfcdb17ad0618710ffd466976af0422b53627bd
Kelly Nelson <kelnel86@gmail.com>
2022-09-22 19:21:07 -0500

    populating stocks page

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -12,8 +12,12 @@ console.log(props)
     const {name, symbol, lastPrice, change, high, low, open} = thisStock;
 
     return (
-        
-        <h1>{name}</h1>
+        <div>
+            <h1>{name}</h1>
+            <h2>{symbol}</h2>
+            <h3>Last Price: {lastPrice}</h3>
+            <h3></h3>
+        </div>
         
     )
 }
