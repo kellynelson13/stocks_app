3ee1e70575afc17e646e41dd8ad187dc1838ff7d
Kelly Nelson <kelnel86@gmail.com>
2022-09-22 19:33:08 -0500

    finishes app

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>Welcome!</h1>
-            <p>Please enjoy our web app where you can click on different stock to see more information about them such as last price, change, high, low and open</p>
+            <p>Please enjoy our web app where you can click on different stock to see more information about them such as last price, change, high, low and open.  <strong>Click on "HOME" to get started!</strong></p>
         </div>
 
     )
